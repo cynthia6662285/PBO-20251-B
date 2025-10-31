5298ae7669bc987410ccf89678721947d8a39ccb
cynthia6662285 <if24.cynthiajuniartien@mhs.ubpkarawang.ac.id>
2025-10-31 13:15:24 +0700

    namespace, import, dan access modifier

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once('./Luaslingkaran.php');
+
+use App\Math\LuasLingkaran;
+
+$lingkaran = new LuasLingkaran(10);
+// $lingkaran->r = 12;
+$lingkaran->tampil('roda'); //method tampil
